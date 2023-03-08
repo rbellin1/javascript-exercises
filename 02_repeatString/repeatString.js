db084c2f6e77b81086eb6dc480072be09474593f
rbellin1 <rbellinger93@gmail.com>
2023-03-08 10:57:55 -0500

    lesson 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
 
+
+const repeatString = function(word, number) {
+    let message = "";
+    if (number < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < number; i++) {
+        message = message + word;
+    }
+
+    return message;
 };
 
 // Do not edit below this line
