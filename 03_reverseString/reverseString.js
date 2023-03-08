68000a38a1d9c80f4b0497e9ac5cfd45d49ea6fa
rbellin1 <rbellinger93@gmail.com>
2023-03-08 11:06:00 -0500

    lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newArray = word.split("");
+    newArray.reverse();
+    let newWord = newArray.join('');
+    return newWord;
 };
 
 // Do not edit below this line
